0d91b0611455feead0529e391afe26239670ba59
JeanOTTO <124897888+JeanOTTO@users.noreply.github.com>
2023-05-24 17:19:54 +0200

    Conception graphique de la page de profil ( NeighbourProfileActivity )

---
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/NeighbourProfileActivity.java
@@ -0,0 +1,16 @@
+package com.openclassrooms.entrevoisins.ui.neighbour_list;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.openclassrooms.entrevoisins.R;
+
+public class NeighbourProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_neighbour_profile);
+    }
+}
